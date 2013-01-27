4c1ff40328bbefca2f8b9e9d8696375ee59daa05
masuda <masuda@my-style.jp>
2013-01-27 18:09:07 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqgridSample::Application.config.secret_token = 'a79a7d6f123ee386a659ec8829a09f649618ca7d68adc883427e7212508dacd17213d5ffe294e216f4e55354f75d7b2e4823bc62e6cb5db6e481750aa6bd50b7'
