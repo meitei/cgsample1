4bb7349ce7c70e2d6b125ee1e4bc2a6a044d68ae
shinichi masuda <masuda@my-style.jp>
2013-03-03 10:32:43 +0900

    Modify Deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :rails_env, 'staging'
 
 require 'bundler/capistrano'
-set :bundle_flags, "--no-deployment --without test development production"
+set :bundle_flags, "--no-deployment --without production"
 
 server "rubycenter.net", :app, :web, :db, :primary => true
 
@@ -11,6 +11,6 @@ set :ssh_options, :port => "32131"
 set :use_sudo, false 
 default_run_options[:pty] = true
 
-namespace :deploy do
-  %w(start restart).each { |name| task name, :roles => :app do mod_rails.restart end }
-end
+#namespace :deploy do
+#  %w(start restart).each { |name| task name, :roles => :app do mod_rails.restart end }
+#end
