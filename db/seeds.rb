5bb8fad718894d9f9a0815f2265a3f1c620de662
tsumori <tsumori@cybergrow.co.jp>
2013-03-09 21:21:57 +0900

    見積単価、見積製品テーブルのテストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+MitsumoriSeihin.create(:mitsumoriNo => 1, :seihinNo =>1, :tanka => 25200, :suryo => 3, :tax => 1260, :kin => 26460, :torokushaId => 1, :koshinshaId => 1)
+MitsumoriTanka.create(:seihinNo => 1, :seihinName => '構造フレーム　金属・木材', :tanka => 25200, :tax => 0.05, :torokushaId => 1, :koshinshaId => 1)
