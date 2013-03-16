1b1e9e073c732be3c3a211d296c72c023dc5fd12
shinichi masuda <masuda@my-style.jp>
2013-03-16 17:54:50 +0900

    Add Migrate

--- db/migrate/20130316083239_change_del_flg_type_to_kokyakus.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDelFlgTypeToKokyakus < ActiveRecord::Migration
-  def up
-  	change_column(:kokyakus, :delFlg, :integer, :null => false, :default => 0)
-  end
-
-  def down
-  	change_column(:kokyakus, :delFlg, :tinyint, :null => false, :default => 0)
-  end
-end
