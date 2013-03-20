563f0d7f1aa7cd3a03caa34105c5c469488d47a7
masuda <masuda@my-style.jp>
2013-03-21 00:51:47 +0900

    Add Sorcery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	authenticates_with_sorcery!
-	  attr_accessible :username, :email, :password, :password_confirmationshainCd, :shainCd, :myoji, :name, :myojiFuri, :nameFuri, :manageFlg, :koshinshaId, :torokushaId
+	  attr_accessible :username, :email, :password, :password_confirmation, :shainCd, :myoji, :name, :myojiFuri, :nameFuri, :manageFlg, :koshinshaId, :torokushaId
 
 	  validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
       validates_confirmation_of :password, :message => "should match confirmation", :if => :password
