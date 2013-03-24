afff70ce5b84dcfad3a0dbfd07a3cec5fc411ea1
tsumori <tsumori@cybergrow.co.jp>
2013-03-25 07:43:00 +0900

    メニュー画面に見積書出力ボタン(サンプル)を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,70 +54,70 @@ end
 ############################################################
 # Test Data
 ############################################################
-# Kokyaku.destroy_all(["1 = ?", 1])
-# Kokyaku.create(
-# 	:kokyakuId => 1,
-# 	:kokyakuNm => '顧客　太郎',
-# 	:torokushaId => 1,
-# 	:koshinshaId => 1
-# )
+#Kokyaku.destroy_all(["1 = ?", 1])
+Kokyaku.create(
+	:kokyakuId => 1,
+	:kokyakuNm => '顧客　太郎',
+	:torokushaId => 1,
+	:koshinshaId => 1
+)
 
 
 # Seihin.destroy_all(["1 = ?", 1])
-# Seihin.create(
-# 	:seihinId => 1,
-# 	:bunruiCd => 1,
-# 	:bunruiNm => '分類１',
-# 	:hinmeiCd => 1,
-# 	:hinmeiNm => '品名１',
-# 	:katashikiCd => 1,
-# 	:katashikiNm => '座位保持装置 2013-スタンダードモデル',
-# 	:torokushaId => 1,
-# 	:koshinshaId => 1
-# )
+Seihin.create(
+	:seihinId => 1,
+	:bunruiCd => 1,
+	:bunruiNm => '分類１',
+	:hinmeiCd => 1,
+	:hinmeiNm => '品名１',
+	:katashikiCd => 1,
+	:katashikiNm => '座位保持装置 型式001',
+	:torokushaId => 1,
+	:koshinshaId => 1
+)
 
 
 # MitsumoriSeihin.destroy_all(["1 = ?", 1])
-# 200.times{|num|
-# 	num = num + 1
-# 	tanka = num * 100
-# 	tax = tanka * num * 0.05
-# 	kin = tanka * num + tax
-# 	MitsumoriSeihin.create(:mitsumoriNo => 1, :seihinNo => num, :tanka => tanka, :suryo => num, :tax => tax, :kin => kin, :torokushaId => 1, :koshinshaId => 1)
-# }
+200.times{|num|
+	num = num + 1
+	tanka = num * 100
+	tax = tanka * num * 0.05
+	kin = tanka * num + tax
+	MitsumoriSeihin.create(:mitsumoriNo => 1, :seihinNo => num, :tanka => tanka, :suryo => num, :tax => tax, :kin => kin, :torokushaId => 1, :koshinshaId => 1)
+}
 
 
 # KonyuRireki.destroy_all(["1 = ?", 1])
-# KonyuRireki.create(
-# 	:konyuRirekiId => 1,
-# 	:kokyakuId => 1,
-# 	:mitsumoriDt => '2013/03/20',
-# 	:mitsumoriTantoEigyoCd => 1,
-# 	# :seihinCd => 1,
-# 	:torokushaId => 1,
-# 	:koshinshaId => 1
-# )
+KonyuRireki.create(
+	:konyuRirekiId => 1,
+	:kokyakuId => 1,
+	:mitsumoriDt => '2013/03/20',
+	:mitsumoriTantoEigyoCd => 1,
+	# :seihinCd => 1,
+	:torokushaId => 1,
+	:koshinshaId => 1
+)
 
 
 # Mitsumori.destroy_all(["1 = ?", 1])
-# Mitsumori.create(
-# 	:konyuRirekiId => 1,
-# 	:kokyakuId => 1,
-# 	:mitsumoriNo => 1,
-# 	:torokushaId => 1,
-# 	:koshinshaId => 1
-# )
+Mitsumori.create(
+	:konyuRirekiId => 1,
+	:kokyakuId => 1,
+	:mitsumoriNo => 1,
+	:torokushaId => 1,
+	:koshinshaId => 1
+)
 
 
-# imageDir = File.join(Rails.root, 'db', 'import', 'images')
+imageDir = File.join(Rails.root, 'db', 'import', 'images')
 # TestImage.destroy_all(["1 = ?", 1])
-# TestImage.create(
-# 	:mitsumoriNo => 1,
-# 	:text => "Sample Text",
-#  	:mainImage1 => File.read(File.join(imageDir, 'char1.jpg')),
-#  	:mainImage2 => File.read(File.join(imageDir, 'char2.png')),
-#  	:subImage1 => File.read(File.join(imageDir, 'face0.png')),
-#  	:subImage2 => File.read(File.join(imageDir, 'face3.png')),
-#  	:subImage3 => File.read(File.join(imageDir, 'face6.png')),
-#  	:subImage4 => File.read(File.join(imageDir, 'face7.png'))
-# )
+TestImage.create(
+	:mitsumoriNo => 1,
+	:text => "Sample Text",
+ 	:mainImage1 => File.read(File.join(imageDir, 'char1.jpg')),
+ 	:mainImage2 => File.read(File.join(imageDir, 'char2.png')),
+ 	:subImage1 => File.read(File.join(imageDir, 'face0.png')),
+ 	:subImage2 => File.read(File.join(imageDir, 'face3.png')),
+ 	:subImage3 => File.read(File.join(imageDir, 'face6.png')),
+ 	:subImage4 => File.read(File.join(imageDir, 'face7.png'))
+)
