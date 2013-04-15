e4e7affc65d57f54776b42f20b3b1e7df32b75e6
masuda <masuda@cybergrow.co.jp>
2013-04-15 13:01:12 +0900

    Mitsumori Modify

--- public/js/mitsumori/wizard10.js
+++ public/js/mitsumori/wizard10.js
@@ -39,19 +39,6 @@
   // OP車椅子フレーム
   function wizard10d(){
     $("button.jqwizard_next").removeAttr("disabled");
-    // 次へ
-    $("button.jqwizard_next").click(function() {
-      // 見積データ保存
-      wm.mitsumoriData["COL19_1"] = $("input[name='COL19_1']:checked").val();
-      step = 11;
-      $("div.statuslabel").html("<span>STEP 11</span>" + $("div#step11 > div#title").text());
-      $("button#jqwizard_next").attr("disabled", "disabled");
-    });
-    // 戻る
-    $("button.jqwizard_previous").click(function() {
-      step = 9;
-      $("div.statuslabel").html("<span>STEP 9</span>" + $("div#step9 > div#title").text());
-    });
     // 単価を書き換え
     updateUnitPrice("step10");
     wm.submitItems[10] = submitItem19;
